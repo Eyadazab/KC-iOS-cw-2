2def60a5f4d00901912c9240ea18495fa4e2457e
Eyadazab <dodyazab2008.kw@gmail.com>
2022-10-14 15:26:02 +0300

    .

--- day2.2/day2/day2/ContentView.swift
+++ day2.2/day2/day2/ContentView.swift
@@ -24,18 +24,77 @@ struct ContentView: View {
                     
                     
                 }.padding()
+                Spacer()
                 HStack{
                     Text("6:46")
-                        .font(.system(size: 90))
+                    
+                        .font(.system(size: 100))
+                    
+                    Text("25")
+                        .padding(.top,50)
+                    
                 }
-            }.foregroundColor(Color.white)
+                Text("باقي على الاذان")
+                
+                Spacer()
+                VStack{
+                    HStack{
+                        Image(systemName: "chevron.left")
+                        Spacer()
+                        Text("21 feb-12")
+                        Spacer()
+                        Image(systemName: "chevron.right")
+                    }    .padding()
+                    Divider()
+                    HStack{
+                        Text("3:37")
+                        Spacer()
+                        Text("الفجر")
+                    }.padding()
+                    HStack{
+                        Text("5:04")
+                        Spacer()
+                        Text("الشروق")
+                    }.padding()
+                    HStack{
+                        Text("11:45")
+                        Spacer()
+                        Text("الظهر")
+                    }.padding()
+                    HStack{
+                        Text("3:21")
+                        Spacer()
+                        Text("العصر")
+                    }.padding()
+                    HStack{
+                        Text("6:25")
+                        Spacer()
+                        Text("المغرب")
+                    }.padding()
+                    HStack{
+                        Text("7:50")
+                        Spacer()
+                        Text("العشاء")
+                    }.padding()
+                    
+                }.background(Color.white.opacity(0.3))            }.foregroundColor(Color.white)
+                
+                
+            
         }
-        
     }
 }
+                    
+                    
+                    
+                    
+                    
+              
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+
